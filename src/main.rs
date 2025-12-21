f477e5a479a2368992a03837d876037c966f8342
Ivan Diachenko <ivan.dyachenko@gmail.com>
2025-12-21 14:54:00 +0100

    Add CORS support to allow all origins and GET method

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,22 @@ struct HelloWorld {
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
+
+    // Configure CORS to allow all origins and GET method
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["GET"])
+        .allow_headers(vec!["content-type"]);
+
     // Match any request and return hello world as JSON!
-    let routes = warp::any().map(|| {
-        let hello = HelloWorld {
-            message: "Hello, World!".to_string(),
-        };
-        warp::reply::json(&hello)
-    });
+    let routes = warp::any()
+        .map(|| {
+            let hello = HelloWorld {
+                message: "Hello, World!".to_string(),
+            };
+            warp::reply::json(&hello)
+        })
+        .with(cors);
 
     info!("start server");
 
