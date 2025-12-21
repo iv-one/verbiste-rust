1ec229c311d4007906be89717c770d9490ba730d
Ivan Diachenko <ivan.dyachenko@gmail.com>
2025-12-21 15:06:05 +0100

    Add verbs module with XML parsing and /verb/:verb endpoint with binary search

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 #![deny(warnings)]
-use log::info;
+mod verbs;
+
+use log::{error, info};
 use serde::Serialize;
-use warp::Filter;
+use std::sync::Arc;
+use warp::{Filter, Rejection, Reply};
+
+async fn get_verb_handler(
+    verb_name: String,
+    verbs: Arc<Vec<verbs::Verb>>,
+) -> Result<warp::reply::Response, Rejection> {
+    match verbs.binary_search_by(|v| v.verb.cmp(&verb_name)) {
+        Ok(index) => Ok(warp::reply::json(&verbs[index]).into_response()),
+        Err(_) => {
+            Ok(warp::reply::with_status("", warp::http::StatusCode::NOT_FOUND).into_response())
+        }
+    }
+}
 
 #[derive(Serialize)]
 struct HelloWorld {
@@ -12,21 +27,47 @@ struct HelloWorld {
 async fn main() {
     pretty_env_logger::init();
 
+    // Load all verbs on startup
+    info!("Loading verbs from data/verbs-fr.xml...");
+    let verbs = match verbs::load_all_verbs() {
+        Ok(v) => {
+            info!("Loaded {} verbs", v.len());
+            Arc::new(v)
+        }
+        Err(e) => {
+            error!("Failed to load verbs: {}", e);
+            std::process::exit(1);
+        }
+    };
+
     // Configure CORS to allow all origins and GET method
     let cors = warp::cors()
         .allow_any_origin()
         .allow_methods(vec!["GET"])
         .allow_headers(vec!["content-type"]);
 
+    // Clone Arc for use in closures
+    let verbs_for_verb_handler = verbs.clone();
+
+    // Handler for /verb/:verb endpoint
+    let verb_route = warp::path("verb")
+        .and(warp::path::param::<String>())
+        .and(warp::get())
+        .and_then(move |verb_name: String| {
+            let verbs = verbs_for_verb_handler.clone();
+            async move { get_verb_handler(verb_name, verbs).await }
+        });
+
     // Match any request and return hello world as JSON!
-    let routes = warp::any()
-        .map(|| {
-            let hello = HelloWorld {
-                message: "Hello, World!".to_string(),
-            };
-            warp::reply::json(&hello)
-        })
-        .with(cors);
+    let hello_route = warp::any().map(|| {
+        let hello = HelloWorld {
+            message: "Hello, World!".to_string(),
+        };
+        warp::reply::json(&hello)
+    });
+
+    // Combine routes
+    let routes = verb_route.or(hello_route).with(cors);
 
     info!("start server");
 
---
+++ src/verbs.rs
@@ -0,0 +1,102 @@
+use quick_xml::Reader;
+use quick_xml::events::Event;
+use serde::Serialize;
+use std::fs::File;
+use std::io::BufReader;
+
+#[derive(Debug, Clone, Serialize)]
+#[allow(dead_code)]
+pub struct Verb {
+    pub verb: String,
+    pub template: String,
+    pub aspirate_h: bool,
+}
+
+pub fn load_all_verbs() -> Result<Vec<Verb>, Box<dyn std::error::Error>> {
+    let file = File::open("data/verbs-fr.xml")?;
+    let reader = BufReader::new(file);
+    let mut xml_reader = Reader::from_reader(reader);
+    xml_reader.trim_text(true);
+
+    let mut verbs = Vec::new();
+    let mut buf = Vec::new();
+    let mut current_verb: Option<String> = None;
+    let mut current_template: Option<String> = None;
+    let mut current_aspirate_h = false;
+    let mut in_v = false;
+    let mut in_i = false;
+    let mut in_t = false;
+
+    loop {
+        match xml_reader.read_event_into(&mut buf) {
+            Ok(Event::Start(e)) => match e.name().as_ref() {
+                b"v" => {
+                    in_v = true;
+                    current_verb = None;
+                    current_template = None;
+                    current_aspirate_h = false;
+                }
+                b"i" => {
+                    if in_v {
+                        in_i = true;
+                    }
+                }
+                b"t" => {
+                    if in_v {
+                        in_t = true;
+                    }
+                }
+                b"aspirate-h" => {
+                    if in_v {
+                        current_aspirate_h = true;
+                    }
+                }
+                _ => {}
+            },
+            Ok(Event::Empty(e)) => {
+                if e.name().as_ref() == b"aspirate-h" && in_v {
+                    current_aspirate_h = true;
+                }
+            }
+            Ok(Event::Text(e)) => {
+                let text = e.unescape()?.to_string();
+                if in_i {
+                    current_verb = Some(text);
+                } else if in_t {
+                    current_template = Some(text);
+                }
+            }
+            Ok(Event::End(e)) => match e.name().as_ref() {
+                b"v" => {
+                    if let (Some(verb), Some(template)) =
+                        (current_verb.take(), current_template.take())
+                    {
+                        verbs.push(Verb {
+                            verb: verb.trim().to_string(),
+                            template: template.trim().to_string(),
+                            aspirate_h: current_aspirate_h,
+                        });
+                    }
+                    in_v = false;
+                    current_aspirate_h = false;
+                }
+                b"i" => {
+                    in_i = false;
+                }
+                b"t" => {
+                    in_t = false;
+                }
+                _ => {}
+            },
+            Ok(Event::Eof) => break,
+            Err(e) => return Err(Box::new(e)),
+            _ => {}
+        }
+        buf.clear();
+    }
+
+    // Sort verbs by verb name for binary search
+    verbs.sort_by(|a, b| a.verb.cmp(&b.verb));
+
+    Ok(verbs)
+}
