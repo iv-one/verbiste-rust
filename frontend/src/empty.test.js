c7e676a0fe088967da40b81d27f177c8935c5c33
Ivan Diachenko <ivan.dyachenko@gmail.com>
2025-12-22 15:40:34 +0100

    Add chai assertion library and example test

--- frontend/src/empty.test.js
+++ frontend/src/empty.test.js
@@ -1,10 +1,17 @@
 import assert from 'assert'
 import { describe, it } from 'node:test'
+import { expect } from 'chai'
 
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1)
+    })
+  })
+
+  describe('#chai', function () {
+    it('should return -1 when the value is not present', function () {
+      expect([1, 2, 3].indexOf(4)).to.equal(-1)
     })
   })
 })
