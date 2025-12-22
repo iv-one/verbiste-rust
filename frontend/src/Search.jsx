a1f8426bb19ca5519fb331c3be2d00821147b7be
Ivan Diachenko <ivan.dyachenko@gmail.com>
2025-12-22 13:11:10 +0100

    Update frontend: add Search component and styling improvements

---
+++ frontend/src/Search.jsx
@@ -0,0 +1,8 @@
+export default function Search() {
+  return (
+    <div>
+      <h1 className='text-xs font-semibold border-b border-gray-200 pb-1 w-md'>Verbiste</h1>
+      <input type="text" placeholder="Search" className='w-full py-2 border-0 bg-transparent focus:outline-none' />
+    </div>
+  )
+}
